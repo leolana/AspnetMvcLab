df8cdbac40ba70c40b01a14e731e3aa69f357d05
Leonardo Lana <leolucas.lana@hotmail.com>
2015-07-25 08:36:51 -0300

    Adicionado filters de exemplos

---
+++ Snips/09-Filters/CameJSONFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace iOBC.ActionFilters
+{
+    internal class CamelJsonFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception != null)
+                return;
+
+            var result = (JsonResult) filterContext.Result;
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+            };
+
+            var serialized = JsonConvert.SerializeObject(result.Data, settings);
+
+            filterContext.Result = new ContentResult
+            {
+                Content = serialized,
+                ContentType = "application/json"
+            };
+        }
+    }
+}
---
+++ Snips/09-Filters/LogUncaughtExceptionFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+using Castle.Core.Logging;
+
+namespace LogHoras.Web.ActionFilters.ApiFilters
+{
+    public class LogUncaughtExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private ILogger _logger = NullLogger.Instance;
+
+        [ExcludeFromCodeCoverage]
+        public ILogger Logger
+        {
+            get { return _logger; }
+            set { _logger = value; }
+        }
+
+        public override bool AllowMultiple
+        {
+            get { return false; }
+        }
+
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var ex = context.Exception;
+            if (ex == null) return;
+
+            Logger.Fatal(ex.Message, ex);
+            context.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+        }
+    }
+}
